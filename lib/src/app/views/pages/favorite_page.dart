ed5b03f6b53719fed3b4fce02ef19dcbd45966ce
alanvamaral <alanamaral158@gmail.com>
2024-07-05 22:07:13 -0300

    add card_favorite_widget

--- lib/src/app/views/pages/favorite_page.dart
+++ lib/src/app/views/pages/favorite_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nasa_api/src/app/views/widgets/card_favorite_widget.dart';
 import 'package:provider/provider.dart';
-import 'package:intl/intl.dart';
 
 import '../../controllers/api_controller.dart';
 import 'fav_image_page.dart';
@@ -22,81 +22,31 @@ class FavoritePage extends StatelessWidget {
                 itemCount: controller.favorite.length,
                 itemBuilder: (context, index) {
                   final favorite = controller.favorite[index];
-                  final formatDate = DateFormat('dd/MM/yyyy');
-                  DateTime data = DateTime.parse(favorite.date!);
+                  DateTime date = DateTime.parse(favorite.date!);
 
                   return GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => FavImagePage(
-                          index: index,
-                          shared: () {
-                            controller.shareImage(favorite.url!);
-                          },
-                          title: favorite.title ?? '',
-                          url: favorite.url!,
-                        ),
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => FavImagePage(
+                            index: index,
+                            shared: () {
+                              controller.shareImage(favorite.url!);
+                            },
+                            title: favorite.title ?? '',
+                            url: favorite.url!,
+                          ),
+                        ));
+                      },
+                      child: CardFavoriteWidget(
+                        url: favorite.url!,
+                        data: date,
+                        removeFavorite: () {
+                          controller.removeFavorite(favorite);
+                        },
+                        sharedImage: () {
+                          controller.shareImage(favorite.url!);
+                        },
                       ));
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(bottom: 16),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 69, 69, 69),
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: ListTile(
-                        contentPadding: const EdgeInsets.all(16),
-                        leading: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Image.network(
-                            favorite.url!,
-                            width: 80,
-                            height: 80,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        title: Text(
-                          favorite.title ?? 'Untitled',
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        subtitle: Text(
-                          'Date: ${formatDate.format(data)}',
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[400],
-                          ),
-                        ),
-                        trailing: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              onPressed: () {
-                                controller.removeFavorite(favorite);
-                              },
-                              icon: const Icon(
-                                Icons.delete,
-                                color: Colors.red,
-                              ),
-                            ),
-                            IconButton(
-                              onPressed: () {
-                                controller.shareImage(favorite.url!);
-                              },
-                              icon: const Icon(
-                                Icons.share,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
                 },
               )
             : Center(
---
+++ lib/src/app/views/widgets/card_favorite_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class CardFavoriteWidget extends StatelessWidget {
+  const CardFavoriteWidget({
+    super.key,
+    required this.url,
+    this.title,
+    required this.data,
+    required this.removeFavorite,
+    required this.sharedImage,
+  });
+
+  final String url;
+  final String? title;
+  final DateTime data;
+  final void Function() removeFavorite;
+   final void Function() sharedImage;
+  @override
+  Widget build(BuildContext context) {
+    final formatDate = DateFormat('dd/MM/yyyy');
+
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 69, 69, 69),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ListTile(
+        contentPadding: const EdgeInsets.all(16),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+          child: Image.network(
+            url,
+            width: 80,
+            height: 80,
+            fit: BoxFit.cover,
+          ),
+        ),
+        title: Text(
+          title ?? '',
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          maxLines: 2,
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          'Date: ${formatDate.format(data)}',
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.grey[400],
+          ),
+        ),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              onPressed: removeFavorite,
+              icon: const Icon(
+                Icons.delete,
+                color: Colors.red,
+              ),
+            ),
+            IconButton(
+              onPressed: sharedImage,
+              icon: const Icon(
+                Icons.share,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
